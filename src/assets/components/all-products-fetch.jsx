e822e81b792eed5730c4d649fd73436f10cddc7b
nimamesbah <nimamesbah85@gmail.com>
2025-06-04 15:35:26 +0430

    router set and page modified

--- src/assets/components/all-products-fetch.jsx
+++ src/assets/components/all-products-fetch.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export default function allFetch({fetchFunc,products}){
+export default function AllFetch({fetchFunc,products}){
     useEffect(()=> fetchFunc(),[])
 
 
@@ -12,7 +12,7 @@ export default function allFetch({fetchFunc,products}){
                     products.map((product)=>{
                         return (
                             <>
-                                <div className="flex flex-col gap-2 justify-between items-center w-52 hover:shadow-[0_0_0_2px_#D10024] py-2 px-4 shadow-[0_0_0_1px_#E4E7ED] h-[450px] text-center cursor-pointer duration-100  ">
+                                <div id={product.id} className="flex flex-col gap-2 justify-between items-center w-52 hover:shadow-[0_0_0_2px_#D10024] py-2 px-4 shadow-[0_0_0_1px_#E4E7ED] h-[450px] text-center cursor-pointer duration-100  ">
                                     <img className="object-contain w-full h-[260px]" src={product.image} alt="" />
                                     <div className="">{product.category}</div>
                                     <div className="break-words">{product.title}</div>
