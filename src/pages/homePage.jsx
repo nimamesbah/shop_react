ef2ef160ebd4f1392c6c7460aa2bcdfbcf075973
nimamesbah <nimamesbah85@gmail.com>
2025-06-30 23:02:22 +0430

    questions updated

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,13 +1,12 @@
 import { useImmer } from 'use-immer'
 import Typography from '@mui/material/Typography';
-import { Button, CircularProgress } from '@mui/material';
+import { Button, CircularProgress, createTheme } from '@mui/material';
 import AllFetch from '../assets/components/all-products-fetch';
 import { useNavigate } from 'react-router-dom';
 import Header from '../assets/components/Header';
 import { useCallback, useEffect } from 'react';
 import axios from 'axios';
-// question section: cartPage, quantity is one amount behind.,why logo gone,cartCards data as {}
-
+// question section: .,why logo gone,cartCards data as {},searchedPage syntax
 
 
 function HomePage() {
@@ -26,6 +25,7 @@ function HomePage() {
       .then(data => {setCategories(data); setIsLoading(false)} )
     },[])
     useEffect(()=> categoriesFetch(),[])
+    useEffect(()=>console.log("theme",createTheme()),[])
     
   
   return (
